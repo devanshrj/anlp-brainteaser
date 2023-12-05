dba1aa3bdeb722c37103f833ea7e3adad8a763e1
Devansh Jain <devanshrj@outlook.com>
2023-12-05 01:01:08 -0500

    flant5 training and inference

--- flant5-inference.py
+++ flant5-inference.py
@@ -1,19 +1,10 @@
 import json
 import pandas as pd
+import torch
 import tqdm
 
+from collections import defaultdict
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
-
-
-model = AutoModelForSeq2SeqLM.from_pretrained(
-    "google/flan-t5-large", device_map="auto")
-tokenizer = AutoTokenizer.from_pretrained(
-    "google/flan-t5-large", device_map="auto")
-# path = f"/usr1/data/devanshj/brainteaser/checkpoints/flan-t5-large_adversarial_finetune"
-# model = AutoModelForSeq2SeqLM.from_pretrained(
-#     path, device_map="auto")
-# tokenizer = AutoTokenizer.from_pretrained(
-#     "google/flan-t5-large", device_map="auto")
 
 
 option2label = {
@@ -27,72 +18,76 @@ option2label = {
     'D': 3
 }
 
-prediction_data = {
-    'id': [],
-    'question': [],
-    'answer': [],
-    'label': [],
-    'prediction': [],
-    'correct': [],
-    'group': [],
-    'choice_0': [],
-    'choice_1': [],
-    'choice_2': [],
-    'choice_3': [],
-}
-
-file_path = "../data/SP-train.json"
+file_path = "data/SP-train.json"
 with open(file_path) as f:
     data = json.load(f)
-print("Number of puzzles: ", len(data))
+print("number of puzzles: ", len(data))
 
 # file_path = "SP-eval.json"
 # with open(file_path) as f:
 #     data = json.load(f)
 # print(len(data))
 
-for mcq in tqdm.tqdm(data):
-    prediction_data['id'].append(mcq['id'])
-    prediction_data['question'].append(mcq['question'])
-    prediction_data['answer'].append(mcq['answer'])
-    prediction_data['label'].append(mcq['label'])
-    prediction_data['choice_0'].append(mcq['choice_list'][0])
-    prediction_data['choice_1'].append(mcq['choice_list'][1])
-    prediction_data['choice_2'].append(mcq['choice_list'][2])
-    prediction_data['choice_3'].append(mcq['choice_list'][3])
-    if 'SR' in mcq['id']:
-        prediction_data['group'].append('SR')
-    elif 'CR' in mcq['id']:
-        prediction_data['group'].append('CR')
-    else:
-        prediction_data['group'].append('OG')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("device:", device)
 
-    prompt = f"""Question: {mcq['question']}
+model_paths = {
+    'flan-t5-large-no-finetune': 'google/flan-t5-large',
+    'flan-t5-large-rs-finetune': '/usr1/data/devanshj/brainteaser/checkpoints/flan-t5-large_full_finetune',
+    'flan-t5-large-adv-finetune': '/usr1/data/devanshj/brainteaser/checkpoints/flan-t5-large_adversarial_finetune',
+}
 
-    What is the correct answer to the question from the following choices?
-    (A) {mcq['choice_list'][0]}
-    (B) {mcq['choice_list'][1]}
-    (C) {mcq['choice_list'][2]}
-    (D) {mcq['choice_list'][3]}"""
+for model_name, model_path in model_paths.items():
+    print("-" * 50)
+    print("model name:", model_name)
+    MODEL_SIZE = model_name.split('-')[2]
+    BASE_MODEL = f"google/flan-t5-{MODEL_SIZE}"
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_path).to(device)
+    tokenizer = AutoTokenizer.from_pretrained(BASE_MODEL)
 
-    inputs = tokenizer(prompt, return_tensors="pt")
-    outputs = model.generate(**inputs)
-    predicted_option = tokenizer.batch_decode(
-        outputs, skip_special_tokens=True)[0]
-    for k, v in option2label.items():
-        if k in predicted_option:
-            predicted_label = v
+    prediction_data = defaultdict(list)
 
-    prediction_data['prediction'].append(predicted_label)
-    if int(mcq['label']) == int(predicted_label):
-        prediction_data['correct'].append(1)
-    else:
-        prediction_data['correct'].append(0)
+    for mcq in tqdm.tqdm(data):
+        prediction_data['id'].append(mcq['id'])
+        prediction_data['question'].append(mcq['question'])
+        prediction_data['answer'].append(mcq['answer'])
+        prediction_data['label'].append(mcq['label'])
+        prediction_data['choice_0'].append(mcq['choice_list'][0])
+        prediction_data['choice_1'].append(mcq['choice_list'][1])
+        prediction_data['choice_2'].append(mcq['choice_list'][2])
+        prediction_data['choice_3'].append(mcq['choice_list'][3])
+        if 'SR' in mcq['id']:
+            prediction_data['group'].append('SR')
+        elif 'CR' in mcq['id']:
+            prediction_data['group'].append('CR')
+        else:
+            prediction_data['group'].append('OG')
 
-# print(prediction_data)
-prediction_df = pd.DataFrame(prediction_data)
-print(prediction_df)
+        prompt = f"""Question: {mcq['question']}
 
-accuracy = prediction_df['correct'].sum() / prediction_df.shape[0]
-print("Overall accuracy: ", accuracy)
-prediction_df.to_csv("SP-train-flant5-11B.csv")
+        What is the correct answer to the question from the following choices?
+        (A) {mcq['choice_list'][0]}
+        (B) {mcq['choice_list'][1]}
+        (C) {mcq['choice_list'][2]}
+        (D) {mcq['choice_list'][3]}"""
+
+        inputs = tokenizer(prompt, return_tensors="pt").to(device)
+        outputs = model.generate(**inputs)
+        predicted_option = tokenizer.batch_decode(
+            outputs, skip_special_tokens=True)[0]
+        for k, v in option2label.items():
+            if k in predicted_option:
+                predicted_label = v
+
+        prediction_data['prediction'].append(predicted_label)
+        if int(mcq['label']) == int(predicted_label):
+            prediction_data['correct'].append(1)
+        else:
+            prediction_data['correct'].append(0)
+
+    prediction_df = pd.DataFrame(prediction_data)
+    accuracy = prediction_df['correct'].sum() / prediction_df.shape[0]
+    print("overall accuracy: ", accuracy * 100)
+
+    print("storing results...")
+    prediction_df.to_csv(f"results/a4/{model_name}.csv")
